ef0b96ec7235d7bd9ebfec2eea928e87b389b6a9
IT21312380 <it21312380@my.sliit.lk>
2025-05-23 01:56:31 +0530

    testing27

--- main.py
+++ main.py
@@ -303,6 +303,6 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Fallback to 8000 for local
+    port = int(os.environ.get("PORT", 8099))  # Fallback to 8000 for local
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
